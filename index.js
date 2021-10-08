2284c6fc7ab3dc6aefe48b145bbceeaf570c79ed
Stephen Gray <stephen_gray1255@yahoo.com>
2021-10-08 14:45:36 +1100

    added js function to render usable data object from GoogleBooksAPI

--- index.js
+++ index.js
@@ -1,40 +1,53 @@
 const GOOGLE_BOOKS = "https://www.googleapis.com/books/v1/volumes?q="
 ;
 const input = document.querySelector("#search");
-let SEARCH = "vonnegut";
-const API_KEY = "key=AIzaSyCTKAC3zFG3p2sr2Ygc2_B_VI-GSs0mqQ0";
+let SEARCH = "slaughterhouse five";
+
 
 const getBooks = async () => {
-  const response = await fetch(`${GOOGLE_BOOKS}${SEARCH}${API_KEY}`);
+  const response = await fetch(`${GOOGLE_BOOKS}${SEARCH}&orderBy=relevance`);
 
   const data = await response.json();
-  console.log(data.data);
-  return data.data;
-  
+  console.log(data);
+  return data.items;
 }
 
 getBooks();
+
+
+const getInfo = async (query) => {
+  const info = await getBooks();
+  console.log(info);
+  const bookArray = info.map((query) => {
+    const bookObject = {
+      title: query.volumeInfo.title,
+      author: query.volumeInfo.authors,
+      description: query.volumeInfo.description
+    }
+    console.log(bookObject);
+    return bookObject;
+  })
+};
+
+getInfo();
 
 const button = document.querySelector("#getBooks");
 
 button.addEventListener("click", async(event) => {
   const books = await getBooks();
-  console.log(books);
+  console.log(books);})
 
-  // const input = document.querySelector ("#pageNumber")
+  const bookItems = () => {
+    const element = document.createElement('li');
+    const bookText = `${book}`;
+    const textNode = document.createTextNode(bookText);
 
-  // const listItems = () => {
-  //   const element = document.createElement('li');
-  //   const bookText = `${book}`;
-  //   const textNode = document.createTextNode(bookText);
+    element.appendChild(textNode);
+    return element;
+  };
 
-  //   element.appendChild(textNode);
-  //   return element;
-  });
+  const list = document.querySelector("#books");
 
-  // const list = document.querySelector("#books");
+  const append = parent => child => parent.appendChild(child);
 
-  // const append = parent => child => parent.appendChild(child);
-
-  // listItems.forEach(append(list));
-// })
+  listItems.forEach(append(list));
