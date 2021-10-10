fb36b6cbdeae76badc2436c98f146041d4867312
Stephen Gray <stephen_gray1255@yahoo.com>
2021-10-10 20:51:34 +1100

    Added javascript functionality and styling

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const GOOGLE_BOOKS = "https://www.googleapis.com/books/v1/volumes?q="
 ;
+const input = document.querySelector("#search");
+const query = input.value; 
 
 
 const getBooks = async (query) => {
@@ -10,32 +12,15 @@ const getBooks = async (query) => {
 
 
 
-// const getInfo = async (query) => {
-//   const info = await getBooks(query);
-//   // console.log(info);
-//   const bookArray = info.map((query) => {
-//     const bookObject = {
-//       title: query.volumeInfo.title,
-//       author: query.volumeInfo.authors,
-//       img: query.volumeInfo.imageLinks,
-//       description: query.volumeInfo.description
-//     }
-//     // console.log(bookObject);
-//     return bookObject;
-//   })
-//   return bookArray;
-// };
-
-// getInfo();
-
 const button = document.querySelector("#getBooks");
 
 let list = {};
 
 button.addEventListener("click", async(event) => {
- 
-  const input = document.querySelector("#search");
-  const query = input.value; 
+  // create function to reset result
+
+  // const input = document.querySelector("#search");
+  // const query = input.value; 
 
   if(!query) {
     alert("Type in a keyword and click search!");
@@ -47,21 +32,55 @@ button.addEventListener("click", async(event) => {
 
   list = gotBooks.map((book) => {
 
-  const element = document.createElement('li');
+  const element = document.createElement('h4');
 
   const cover = document.createElement('img');
-  cover.src = `${book.volumeInfo.imageLinks.smallThumbnail}`;
+  cover.classList.add("cover");
+  if (!book.volumeInfo.imageLinks){
+    cover.src = "./images/default_book_cover.jpg";      
+  } else {
+    cover.src = `${book.volumeInfo.imageLinks.smallThumbnail}`;
 
-  const bookText = `${book.volumeInfo.title} written by ${book.volumeInfo.authors}`;
-  const textNode = document.createTextNode(bookText);
+  };
+
+  const titleText = `${book.volumeInfo.title}`;
+  const titleNode = document.createTextNode(titleText);
+
+
+  const description = document.createElement('p');
+  const descriptionText = " " + ` Description: ${book.volumeInfo.description}`;
+  const descriptionNode = document.createTextNode(descriptionText);
+  description.appendChild(descriptionNode);
+
+  const author = document.createElement('h5');
+  const authorText = ` Written by: ${book.volumeInfo.authors}`;
+  const authorNode = document.createTextNode(authorText);
+  author.appendChild(authorNode);
+  
 
   element.appendChild(cover);
-  element.appendChild(textNode);
+  element.appendChild(titleNode);
+  element.appendChild(author)
+  // element.appendChild(description);
   console.log(element)
+  element.classList.add("gridItem");
   return element; 
   });
 
+const gridList = document.querySelector(".books__grid__list");
+
+const append = (parent) => (child) => parent.appendChild(child);
+
+list.forEach(append(gridList));
+
+if (gridList.innerHTML !== "") {
+  gridList.innerHTML = "";
+  list.forEach(append(gridList));
+} else {
+  list.forEach(append(gridList));
+}
 });
+
 
  
 
