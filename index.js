cabf0d6a6639a6786ff8627e120c28dbdbd69e01
Stephen Gray <stephen_gray1255@yahoo.com>
2021-10-12 17:40:56 +1100

    fixed styling

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const GOOGLE_BOOKS = "https://www.googleapis.com/books/v1/volumes?q="
-;
-
-
-
-const getBooks = async (query) => {
-  const response = await fetch(`${GOOGLE_BOOKS}${query}&orderBy=relevance&maxResults=8`);
-  const data = await response.json();
-  return data.items;
-}
-
-
+import getBooks from "./dom-utils.js"
 
 const button = document.querySelector("#getBooks");
 
